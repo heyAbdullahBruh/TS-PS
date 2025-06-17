d1e920bfbfe76714860cf1fb7105ca987c6b8c24
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 15:58:38 +0600

    TS-PS-1 DeclareVariable

---
+++ DeclareVariable.js
@@ -0,0 +1,10 @@
+var userName = "Hellow World";
+// userName = 'Gellow Borld'  //This isn't editable .Bcz, userName is constant variable
+console.log(userName); // Hellow World
+var age;
+age = 12;
+console.log(age); //12
+age = 24.4;
+console.log(age); //24.5
+age = 34;
+console.log(age); //34
---
+++ DeclareVariable.ts
@@ -0,0 +1,13 @@
+const userName: string = "Hellow World";
+// userName = 'Gellow Borld'  //This isn't editable .Bcz, userName is constant variable
+
+console.log(userName); // Hellow World
+
+let age: string | number;
+
+age = 12;
+console.log(age); //12
+age = 24.4;
+console.log(age); //24.5
+age = 34;
+console.log(age); //34
