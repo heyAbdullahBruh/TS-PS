45cd99b607c28b380b79545851cd1eedb5f2b9bc
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 16:17:37 +0600

    TS-PS-1.7 arraysum

--- bascisTypeAndVar.js
+++ bascisTypeAndVar.js
@@ -1,11 +1,8 @@
 /* Declare TS type & variable ---> */
-const userName: string = "Hellow World";
+var userName = "Hellow World";
 // userName = 'Gellow Borld'  //This isn't editable .Bcz, userName is constant variable
-
 console.log(userName); // Hellow World
-
-let age: string | number; // <--- unions type
-
+var age; // <--- unions type
 age = 12;
 console.log(age); //12
 age = 24.4;
@@ -14,27 +11,21 @@ age = 34;
 console.log(age); //34
 age = "Chourannobboi";
 console.log(age); //Chourannobbois
-
 /* Make Square Function ---> */
-const squareFunc = (num: number) => {
-  return num ** 2;
+var squareFunc = function (num) {
+    return Math.pow(num, 2);
 };
-
-const threeSqr = squareFunc(1000);
+var threeSqr = squareFunc(1000);
 console.log(threeSqr);
-
 /**🔹 Create an array of numbers and write a function that returns the sum of all numbers. */
-
-function arraySum(array: number[]) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum = sum + array[i];
-  }
-
-  return sum;
+function arraySum(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum;
 }
-
-const numArr = [
-  1, 2, 3, 4, 5, 6, 29323, 4234, 23, 4, 2, 34, 235, 4, 34, 23, 4, 2342,
+var numArr = [
+    1, 2, 3, 4, 5, 6, 29323, 4234, 23, 4, 2, 34, 235, 4, 34, 23, 4, 2342,
 ];
 console.log(arraySum(numArr)); //36283
