8ac8520dc80d908b505755e22da4814684b004b8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 16:34:41 +0600

    TS-PS-3 FunctionTyping.ts-2 Define add function

--- 4FunctionTyping.js
+++ 4FunctionTyping.js
@@ -3,3 +3,13 @@ function greetUser(name) {
     return "Hellow ".concat(name);
 }
 console.log(greetUser("Yorld")); // Hellow Yorld
+/**🔹 Define a function add(a: number, b: number) and return their sum. Type everything properly. */
+function add(a, b) {
+    return {
+        sum: a + b,
+    };
+}
+console.log(add(0, 0)); // 0
+console.log(add(NaN, NaN)); //NaN
+console.log(add(NaN, 1)); //NaN
+console.log(add(123, 1)); //124
--- 4FunctionTyping.ts
+++ 4FunctionTyping.ts
@@ -4,3 +4,15 @@ function greetUser(name: string) {
 }
 
 console.log(greetUser("Yorld")); // Hellow Yorld
+
+/**🔹 Define a function add(a: number, b: number) and return their sum. Type everything properly. */
+function add(a: number, b: number) {
+  return {
+    sum: a + b,
+  };
+}
+
+console.log(add(0, 0)); // 0
+console.log(add(NaN, NaN)); //NaN
+console.log(add(NaN, 1)); //NaN
+console.log(add(123, 1)); //124
