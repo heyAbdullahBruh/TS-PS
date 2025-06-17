6c4040d1c14ca048fc43b0f0dadd7ca332ba7e52
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 17:15:06 +0600

    TS-PS-4.5 5InterfaceObjectType add optional & readonly properties

--- class/5InterfaceObjectType.js
+++ class/5InterfaceObjectType.js
@@ -1,22 +1,32 @@
 /*
- *🔹 Create an interface User with fields: name (string), age (number), and isActive (boolean). Then create a user object using it.
- *🔹 Write a function printUser(user: User): void that prints the user info.
+ 🔹1 Create an interface User with fields: name (string), age (number), and isActive (boolean). Then create a user object using it.
+ 🔹2 Write a function printUser(user: User): void that prints the user info.
+ 🔹3 Modify the User interface so age is optional.
+ 🔹4 Add a readonly id: number property in the User interface and test assigning a value.
  */
 var User = /** @class */ (function () {
-    function User(name, age, isActive) {
+    function User(id, name, isActive, age) {
+        this.id = id;
         this.name = name;
-        this.age = age;
+        // Only assign age if it's provided
+        if (age !== undefined) {
+            this.age = age;
+        }
         this.isActive = isActive;
     }
     User.prototype.printUser = function () {
+        //🔹2 Write a function printUser(user: User)
         console.log({
+            id: this.id,
             name: this.name,
-            age: this.age,
+            age: this.age || NaN,
             isActive: this.isActive,
         });
     };
     return User;
 }());
-var user1 = new User("Hello", 121, true);
-console.log(user1); // User { name: 'Hello', age: 121, isActive: true }
-user1.printUser(); // { name: 'Hello', age: 121, isActive: true }
+var user1 = new User(61235612, "Hello", true);
+var user2 = new User(11674365, "Mello", false, 25);
+console.log(user1); // User {id: 61235612, name: 'Hello', age: NaN, isActive: true }
+user1.printUser(); // { id: 61235612,name: 'Hello', age: NaN, isActive: true }
+user2.printUser(); // {  id: 11674365,name: 'Mello', age: 25, isActive: false }
