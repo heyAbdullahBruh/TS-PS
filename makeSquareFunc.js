fd606af357d3ea8c3b61357356cf322d4c702e76
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 16:04:15 +0600

    TS-PS-2 squareFucn

---
+++ makeSquareFunc.js
@@ -0,0 +1,5 @@
+var squareFunc = function (num) {
+    return Math.pow(num, 2);
+};
+var threeSqr = squareFunc(1000);
+console.log(threeSqr);
---
+++ makeSquareFunc.ts
@@ -0,0 +1,6 @@
+const squareFunc = (num: number) => {
+  return num ** 2;
+};
+
+const threeSqr=squareFunc(1000);
+console.log(threeSqr);
