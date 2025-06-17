71cc0e9e39379d96b8bb745674b1547be71c5ebf
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-17 16:40:06 +0600

    TS-PS-3 FunctionTyping.ts-3 define Buy result message func

--- 4FunctionTyping.js
+++ 4FunctionTyping.js
@@ -13,3 +13,10 @@ console.log(add(0, 0)); // 0
 console.log(add(NaN, NaN)); //NaN
 console.log(add(NaN, 1)); //NaN
 console.log(add(123, 1)); //124
+/**🔹 Create a function that accepts a string and a number. Return a string like: "You bought 3 apples" */
+var BuyResult = function (itemName, itemCount) {
+    return {
+        message: "You bought ".concat(itemCount, " ").concat(itemName, "s"),
+    };
+};
+console.log(BuyResult("pen", 5)); // { message: 'You bought 5 pens' }
--- 4FunctionTyping.ts
+++ 4FunctionTyping.ts
@@ -16,3 +16,12 @@ console.log(add(0, 0)); // 0
 console.log(add(NaN, NaN)); //NaN
 console.log(add(NaN, 1)); //NaN
 console.log(add(123, 1)); //124
+
+/**🔹 Create a function that accepts a string and a number. Return a string like: "You bought 3 apples" */
+const BuyResult = (itemName: string, itemCount: number) => {
+  return {
+    message: `You bought ${itemCount} ${itemName}s`,
+  };
+};
+
+console.log(BuyResult("pen", 5)); // { message: 'You bought 5 pens' }
